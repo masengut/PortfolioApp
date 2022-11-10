eddef2cd6d802af4f654bf9388e758f6eb213cc4
Masen Gutormson <masengutormson@gmail.com>
2022-11-10 22:01:26 +0000

    Problems hopefully fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "projects#index"
   resources :projects
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # Added for commit
   # Defines the root path route ("/")
   # root "articles#index"
 end
